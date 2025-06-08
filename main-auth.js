8c1284d039bdb9a302fa769cd8d0cd677ae55557
zGabiru <gabiru@gabiru.intelbras.local>
2025-06-08 09:59:32 -0300

    mudando api para render

--- main-auth.js
+++ main-auth.js
@@ -1,5 +1,5 @@
 // URL base do seu backend Flask
-const API_BASE_URL = 'https://acab-45-168-77-154.ngrok-free.app'; // Alterado para o endereço local padrão do Flask
+const API_BASE_URL = 'https://site-alan-back.onrender.com/'; // Alterado para o endereço local padrão do Flask
 
 // --- Funções de Navegação ---
 
